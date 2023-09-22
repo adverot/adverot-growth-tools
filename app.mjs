863b4289fed0862de9cbb6061e634fa9c409bb5d
adverot <ad.verot@gmail.com>
2023-09-22 09:05:51 +0200

    Version OK en local

--- app.mjs
+++ app.mjs
@@ -1,13 +1,30 @@
 import buildMails from './utils/buildMails.js';
 import removeAccents from './utils/removeAccents.js';
 
-document.getElementById('email-checker').addEventListener('submit', (event) => {
+document.getElementById('email-checker').addEventListener('submit', async (event) => {
     event.preventDefault();
     const zoneResultat = document.getElementById('resultat');
     const prenom = document.getElementById('prenom').value;
     const nom = document.getElementById('nom').value;
     const domain = document.getElementById('domaine').value;
     zoneResultat.innerText = '';
+    let message = '';
     const mails = buildMails(removeAccents(prenom), removeAccents(nom));
-    console.log(mails);
+    const mailsQuery = mails.map(mail => `mails=${encodeURIComponent(mail)}`).join('&');
+    const url = `http://localhost:8888/.netlify/functions/findMail?${mailsQuery}&domain=${encodeURIComponent(domain)}`;
+    const response = await fetch(url).then(response => response.json());
+    switch (response.mxStatus) {
+        case 'NO_MX':
+            message = `Le domaine ${domain} n'a pas d'enregistrement MX valide.`
+            break;
+        case 'ACCEPT_ALL':
+            message = `Le domaine ${domain} est paramétré en Accept All.`
+            break;
+        case 'OK':
+            message = response.mail;
+            break;
+        default:
+            message = '!! ERREUR !!'
+    }
+    zoneResultat.innerText = message;
 });
--- netlify/functions/findMail.mjs
+++ netlify/functions/findMail.mjs
@@ -1,21 +1,28 @@
 import legit from 'legit';
 import { ping } from 'smtp-ping';
 
-export default async function findMail(mails, domain) {
+export const handler = async (event, context) => {
+    const mails = event.multiValueQueryStringParameters.mails;
+    const domain = event.multiValueQueryStringParameters.domain;
+    let mxStatus = '';
+    let theMail = '';
+
     try {
         const legitData = await legit('adrien.verot@' + domain);
-        if (!legitData.isValid) {
-            return `Pas d'enreistrement MX valide pour le domaine ${domain}.`;
+        if (!legitData.isValid || legitData.mxArray.length === 0) {
+            mxStatus = 'NO_MX';
         } else {
             const pingData = await ping('adrien.verot@' + domain);
             if (pingData.status !== 'INVALID') {
-                return `Le domaine ${domain} est paramétré en Accept All.`;
+                mxStatus = 'ACCEPT_ALL';
             } else {
-                for (let mail in mails) {
+                mxStatus = 'OK';
+                for (let mail of mails) {
                     try {
                         const pingData = await ping(mail + '@' + domain);
                         if(pingData.status === 'OK') {
-                            return mail + '@' + domain;
+                            theMail = mail + '@' + domain;
+                            break;
                         }
                     } catch (error) {
                         console.log(error);
@@ -25,5 +32,13 @@ export default async function findMail(mails, domain) {
         }
     } catch (error) {
         console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            mxStatus: mxStatus,
+            mail: theMail
+        })
     }
 }
